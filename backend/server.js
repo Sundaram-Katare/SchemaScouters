6f0a6d0fecc9af26c5181af51ed37731b50acc91
Sundaram-Katare <sundaramkatare53@gmail.com>
2026-03-30 14:57:47 +0530

    init

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+    console.log('Server is runnig on PORT 5000');
+});
+
